f3be21cd250c37a067ad27eb123abed0a76c4453
grueq <enwoo5121@gmail.com>
2025-10-11 22:33:31 +0900

    행렬곱 속도 개선

--- NeualNetworks/Matrix.hpp
+++ NeualNetworks/Matrix.hpp
@@ -7,7 +7,7 @@
 #include <cassert>
 #include <cstddef>
 #include <cstdint>
-
+#include <memory>
 using namespace std;
 
 namespace vsnn {
@@ -19,11 +19,16 @@ namespace vsnn {
 	private:
 		i32 rows_ = 0, cols_ = 0;
 		vector<f32> data_;
+		mutable std::unique_ptr<Matrix> transpose_cache_;
+		mutable bool transpose_dirty_ = true;
 	public:
 		Matrix() = default;
 		Matrix(i32 r, i32 c) { Reset(r, c); }
 		void Reset(i32 r, i32 c) {
-			rows_ = r; cols_ = c; data_.assign(static_cast<size_t>(r) * c, 0.0f);
+			rows_ = r; cols_ = c;
+			data_.assign((size_t)r * c, 0.0f);
+			transpose_cache_.reset();
+			transpose_dirty_ = true;
 		}
 		inline i32 Rows() const { return rows_; }
 		inline i32 Cols() const { return cols_; }
@@ -34,5 +39,38 @@ namespace vsnn {
 		inline void Fill(f32 v) { fill(data_.begin(), data_.end(), v); }
 		inline const vector<f32>& Raw() const { return data_; }
 		inline vector<f32>& Raw() { return data_; }
+
+		//전치행렬
+		const Matrix& Transposed() const {
+			if (!transpose_cache_ || transpose_dirty_) {
+				transpose_cache_ = std::make_unique<Matrix>(cols_, rows_);
+				Matrix& T = *transpose_cache_;
+				for (i32 i = 0; i < rows_; ++i) {
+					const f32* src = &data_[(size_t)i * cols_];
+					for (i32 j = 0; j < cols_; ++j)
+						T(j, i) = src[j];
+				}
+				transpose_dirty_ = false;
+			}
+			return *transpose_cache_;
+		}
+
+		Matrix(const Matrix& other) {
+			rows_ = other.rows_;
+			cols_ = other.cols_;
+			data_ = other.data_;
+			transpose_cache_.reset();
+			transpose_dirty_ = true;
+		}
+		Matrix& operator=(const Matrix& other) {
+			if (this == &other) return *this;
+			rows_ = other.rows_;
+			cols_ = other.cols_;
+			data_ = other.data_;
+			transpose_cache_.reset();
+			transpose_dirty_ = true;
+			return *this;
+		}
+		void MarkDirty() { transpose_dirty_ = true; }
 	};
 }
--- NeualNetworks/Ops.hpp
+++ NeualNetworks/Ops.hpp
@@ -4,22 +4,61 @@
 #pragma once
 #include <cmath>
 #include <algorithm>
+#include <immintrin.h>
+#include <thread>
 #include "Matrix.hpp"
 
 using namespace std;
 
 namespace vsnn {
+	//블록 사이즈 설정 (CPU마다 최적값이 다르므로 32, 64, 128 등 2^n으로 변경해가며 실행해봐야 함)
+	const int BLOCK = 128;
+
 	class Ops {
 	public:
+		static inline float Dot(const float* a, const float* b, int len) {
+
+#ifdef __AVX2__
+			int k = 0;
+			__m256 acc = _mm256_setzero_ps();
+			for (; k + 8 <= len; k += 8) {
+				__m256 va = _mm256_loadu_ps(a + k);
+				__m256 vb = _mm256_loadu_ps(b + k);
+				acc = _mm256_fmadd_ps(va, vb, acc);
+			}
+			float tmp[8];
+			_mm256_storeu_ps(tmp, acc);
+			float s = tmp[0] + tmp[1] + tmp[2] + tmp[3] + tmp[4] + tmp[5] + tmp[6] + tmp[7];
+			for (; k < len; ++k) s += a[k] * b[k];
+			return s;
+#else
+			float s = 0.0f;
+			for (int k = 0; k < len; ++k) s += a[k] * b[k];
+			return s;
+#endif
+		}
 		// Y = X * W with shapes: (N,in) * (in,out) = (N,out)
-		static void MatMul(const Matrix& X, const Matrix& W, Matrix& Y) {
-			assert(X.Cols() == W.Rows());
-			if (Y.Rows() != X.Rows() || Y.Cols() != W.Cols()) Y.Reset(X.Rows(), W.Cols());
-			for (i32 n = 0; n < X.Rows(); ++n) {
-				for (i32 j = 0; j < W.Cols(); ++j) {
-					float acc = 0.0f;
-					for (i32 k = 0; k < X.Cols(); ++k) acc += X(n, k) * W(k, j);
-					Y(n, j) = acc;
+		static void MatMul(const Matrix& A, const Matrix& B, Matrix& C) {
+			int M = A.Rows(), K = A.Cols(), N = B.Cols();
+			//캐시 효율성 최적화를 위해 B 행렬 전치
+			const Matrix& Bt = B.Transposed();
+			C.Reset(M, N);
+
+			//캐시 블로킹을 통한 데이터 재사용률 극대화
+			for (int i0 = 0; i0 < M; i0 += BLOCK) {
+				int iMax = std::min(i0 + BLOCK, M);
+				for (int j0 = 0; j0 < N; j0 += BLOCK) {
+					int jMax = std::min(j0 + BLOCK, N);
+					for (int k0 = 0; k0 < K; k0 += BLOCK) {
+						int kMax = std::min(k0 + BLOCK, K);
+						for (int i = i0; i < iMax; ++i) {
+							const float* a = &A.Raw()[(size_t)i * K + k0];
+							for (int j = j0; j < jMax; ++j) {
+								const float* b = &Bt.Raw()[(size_t)j * Bt.Cols() + k0];
+								C(i, j) += Dot(a, b, kMax - k0);
+							}
+						}
+					}
 				}
 			}
 		}
